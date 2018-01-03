abdc2207e78b18eefa3bdeb24208d028d6fc0155
Jana <janamagdeska11@gmail.com>
2018-01-03 18:31:14 +0100

    Local changes Jana

--- test_api.py
+++ test_api.py
@@ -7,12 +7,12 @@ url = "http://localhost:8000/face_detection/predict/"
 
 # load our image and now use the face detection API to find faces in
 # images by uploading an image directly
-image = cv2.imread("test-b.png")
+image = cv2.imread("test.png")
 # hist = hog(image, orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1), visualise=False)
-payload = {"image": open("test-b.png", "rb")}
+payload = {"image": open("test.png", "rb")}
 r = requests.post(url, files=payload).json()
-print "test-b.png: {}".format(r)
+print "test.png: {}".format(r)
 
 # show the output image
-cv2.imshow("test-b.png", image)
+cv2.imshow("test.png", image)
 cv2.waitKey(0) 
